5af6e917ee760d19a9f99b417dbbaf459f0ae315
fernando-ph <fernandolumbangaol11@gmail.com>
2023-05-23 22:12:56 +0700

    shsh

--- coki/god1.js
+++ coki/god1.js
@@ -54,6 +54,7 @@ function addProject(event) {
     checkboxNextjs,
     checkboxReactjs,
     checkboxTypescript,
+    postAt: new Date(),
   };
 
   dataProject.push(project);
@@ -61,6 +62,7 @@ function addProject(event) {
 
   renderProject();
 }
+
 
 function renderProject() {
   document.getElementById("project").innerHTML = "";
@@ -70,8 +72,12 @@ function renderProject() {
     <div class="grid-item1">
           <img src="${dataProject[index].image}" alt="gambar-hehe" />
           <div class="judul-project-deadline">
-            <a href="my-project-content.html"><p class="judul-project">${dataProject[index].projectName}</p></a>
-            <p class="deadline-project">Duration: ${dataProject[index].durasi} bulan</p>
+            <a href="#"><p class="judul-project">${
+              dataProject[index].projectName
+            }</p></a>
+            <p class="deadline-project">Duration: ${
+              dataProject[index].durasi
+            } bulan</p>
           </div>
           <div class="project-detail">
             <p>
@@ -83,6 +89,10 @@ function renderProject() {
             ${dataProject[index].checkboxNextjs}
             ${dataProject[index].checkboxReactjs}
             ${dataProject[index].checkboxTypescript}
+            <p style="font-size: 15px; color: grey; letter-spacing: 0px;">${getDistanceTime(
+              dataProject[index].postAt
+            )} | ${getFullTime(dataProject[index].postAt)}
+            </p>
           </div>
           <div class="btn-group-post-project">
             <button>Edit</button>
@@ -92,3 +102,75 @@ function renderProject() {
         `;
   }
 }
+// cara mencari waktu saat post
+function getFullTime(time) {
+  // console.log("get full time");
+
+  // full time
+  // let time = new Date();
+  // console.log(time);
+
+  // mencari waktu spesifik dd-mm-yy hours:minutes
+  let monthName = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Des",
+  ];
+
+  let date = time.getDate();
+  let monthIndex = time.getMonth();
+  let year = time.getFullYear();
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+
+  if (hours <= 9) {
+    hours = "0" + hours;
+  }
+  if (minutes <= 9) {
+    minutes = "0" + minutes;
+  }
+
+  return `${date}-${monthName[monthIndex]}-${year} ${hours}:${minutes} WIB`;
+}
+
+// mencari jarak antara waktu post dan waktu sebenarnya
+function getDistanceTime(time) {
+  let timeNow = new Date();
+  let timePost = time;
+
+  let distance = timeNow - timePost;
+
+  let milisecond = 1000; // 1detik in ms
+  let secondInHours = 3600; // 1jam in second
+  let hoursInDay = 24; // 1day in hours
+
+  let distanceDay = Math.floor(
+    distance / (milisecond * secondInHours * hoursInDay)
+  );
+  let distanceHours = Math.floor(distance / (milisecond * 60 * 60));
+  let distanceMinutes = Math.floor(distance / (milisecond * 60));
+  let distanceSecond = Math.floor(distance / milisecond);
+
+  if (distanceDay > 0) {
+    return `${distanceDay} Day Ago`;
+  } else if (distanceHours > 0) {
+    return `${distanceHours} Hours Ago`;
+  } else if (distanceMinutes > 0) {
+    return `${distanceMinutes} Minutes Ago`;
+  } else {
+    return `${distanceSecond} Seconds Ago`;
+  }
+}
+
+// set real time distancetime
+// setInterval(function () {
+//   renderProject();
+// }, 2000);
